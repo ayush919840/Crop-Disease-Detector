d9a5251949b2eeda64b090a2320a91122a95054f
ayush919840 <ayushpandey9961@gmail.com>
2025-06-06 22:55:23 +0530

    Add files via upload

---
+++ cnn_model_3.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+import os
+
+train_dir = 'dataset/Train'
+val_dir = 'dataset/Validation'
+
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
+BATCH_SIZE = 32
+EPOCHS = 15
+
+train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        rotation_range=30,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest'
+    )
+
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_data = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical',
+        shuffle=True
+)
+
+val_data = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical',
+        shuffle=True
+)
+
+model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+        MaxPooling2D(2, 2),
+
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+
+        Conv2D(128, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+
+        Flatten(),
+        Dense(256, activation='relu'),
+        Dropout(0.5),  # Dropout for regularization
+        Dense(train_data.num_classes, activation='softmax')  # Softmax for multi-class classification
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+checkpoint = ModelCheckpoint('cnn_model_multi_class.h5', monitor='val_accuracy', save_best_only=True, verbose=1)
+early_stop = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+history = model.fit(
+        train_data,
+        epochs=EPOCHS,
+        validation_data=val_data,
+        callbacks=[checkpoint, early_stop]
+)
+
+model.save('cnn_model__multi_class.h5')
